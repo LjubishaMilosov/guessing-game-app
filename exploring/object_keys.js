a8ba8428d8c1573aea361e8cfd1f79c08095912d
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-23 01:12:18 +0800

    object key and values

---
+++ exploring/object_keys.js
@@ -0,0 +1,11 @@
+const frenchEnglish = {
+    salut: 'hello',
+    evident: 'obvious',
+}
+
+const objectKeys = Object.keys(frenchEnglish)
+console.log(objectKeys)
+
+
+const objectValues = Object.values(frenchEnglish)
+console.log(objectValues)
