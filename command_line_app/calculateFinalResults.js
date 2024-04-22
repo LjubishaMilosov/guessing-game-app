b24eed8100624df71a54df898420a67e51f6bd60
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-23 01:48:32 +0800

    calculate final results part2

--- command_line_app/calculateFinalResults.js
+++ command_line_app/calculateFinalResults.js
@@ -3,12 +3,24 @@ const { gatherResults } = require("./gatherResults.js")
 
 
 const calculateFinalResults = () => {
+    const results = {}
+
     gameState.rounds.forEach((round, index) => {
             const  roundResults = gatherResults(index)
             console.log(roundResults, index)
 
-    })
+            const playerNames = Object.keys(roundResults)
 
+            playerNames.forEach((player) => {
+                if(results[player] === undefined) {
+                    results[player] = roundResults[player]
+                } else {
+                    results[player] = results[player] + roundResults[player]
+                }
+            })
+
+    })
+    return results
 
 }
 
