08e2a2d5294aa16743e773f3253b71d6e649ec2b
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-22 16:01:24 +0200

    third round

--- command_line_app/index.js
+++ command_line_app/index.js
@@ -35,10 +35,8 @@ console.log(`The name of the game is ${gameState.gameName}`);
 console.log(`The players names are ${gameState.players}`);
 
 startRound(0)
-
 makeGuess(0,playerOne, 1969)
 makeGuess(0, playerTwo, 1978)
-
 console.log(JSON.stringify(gameState, undefined, 2));
 const firstRoundResults = gatherResults(0);
 console.log(JSON.stringify(firstRoundResults, undefined, 2))
@@ -49,3 +47,10 @@ makeGuess(1, playerTwo, 300)
 console.log(JSON.stringify(gameState, undefined, 2));
 const seconsRoundResults = gatherResults(1);
 console.log(JSON.stringify(seconsRoundResults, undefined, 2))
+
+startRound(2)
+makeGuess(2, playerOne, 600)
+makeGuess(2, playerTwo, 800)
+console.log(JSON.stringify(gameState, undefined, 2));
+const thirdRoundResults = gatherResults(2);
+console.log(JSON.stringify(thirdRoundResults, undefined, 2))
