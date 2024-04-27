0c8605cbc8bfac3d88b4b59516446dfce091f09e
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-27 18:48:08 +0200

    state handler

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express"
+import { helloHandler } from "./handlers/hello.js"
+import { stateHandler } from "./handlers/state.js"  
 
 const app = express()
 
-app.get("/hello", (req, res) => {
-    res.status(404).send("hello from our first app")
-})
+app.get("/hello", helloHandler)
 
 console.log("Launching app on port 3331")
 app.listen(3331)
+
+app.get("/state", stateHandler)
