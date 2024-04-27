2f0d5c2a6e1fa96bb10aa0f602db82f226c6ec9a
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-27 22:03:01 +0200

    modify gameState and add player endpoint

--- backend/handlers/addPlayer.js
+++ backend/handlers/addPlayer.js
@@ -1,4 +1,25 @@
+
+import { gameState } from "../gameState/index.js"
+
+
 export const addPlayerhandler = (req, res) => {
     console.log("inside addPlayerHandler")
-    console.log(req.body)
+    console.log("request body: ", req.body)
+
+
+    if(!req.body.name){
+        res.status(400).send({
+            message: "name is required"
+        })
+        return
+    }
+
+    gameState.players.push(
+        {
+            name: req.body.name,
+            points: 0,
+        })
+
+        res.status(201).send(gameState.players)
 }
+
