100bc63e0b7736d354f9811cad607d44e0f31ea4
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-04-29 21:26:13 +0800

    returning http errors

--- backend/handlers/addPlayer.js
+++ backend/handlers/addPlayer.js
@@ -14,6 +14,17 @@ export const addPlayerhandler = (req, res) => {
         return
     }
 
+    const playerWithSameName = gameState.players.find((player) => {
+        return player.name === req.body.name
+    })
+
+    if(playerWithSameName){
+        res.status(400).send({
+            message: `There is already a player with the name '${req.body.name}'`
+        })
+        return
+    }
+
     gameState.players.push(
         {
             name: req.body.name,
@@ -21,5 +32,6 @@ export const addPlayerhandler = (req, res) => {
         })
 
         res.status(201).send(gameState.players)
+        return
 }
 
