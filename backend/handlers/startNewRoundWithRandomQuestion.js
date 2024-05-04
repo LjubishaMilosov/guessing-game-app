383015189989dc9749894192dbaa644cf6df95fb
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-05 01:46:33 +0800

    use filter and find to choose not yet asked questions

--- backend/handlers/startNewRoundWithRandomQuestion.js
+++ backend/handlers/startNewRoundWithRandomQuestion.js
@@ -2,19 +2,31 @@ import { gameState } from "../gameState/index.js"
 import { v4 as uuidv4 } from "uuid"
 import { questions } from "../questions.js"
 
+const hasQuestionNotBeenAsked = () => {
+    const hasBeenAsked = gameState.rounds.find( (round) => {
+        return questions.id === round.questionId
+    })
+    return hasBeenAsked === undefined
+}
+
 export const startNewRoundWithRandomQuestion = (request, response) => {
+
+    const notYetAskedQuestions = questions.filter( (question) => {
+        return hasQuestionNotBeenAsked(question)
+    })
+
     
     const randomIndex = Math.floor(Math.random() * questions.length)
     
     const newRound = {
         id: uuidv4(),
-        questionId: questions[randomIndex].id,
+        questionId: notYetAskedQuestions[randomIndex].id,
     }
 
     gameState.rounds.push(newRound)
 
     return response.status(200).send({
         round: newRound,
-        question: questions[randomIndex],
+        question: notYetAskedQuestions[randomIndex],
     })
 }
