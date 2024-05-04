75fda9cef402844b434eb1f162f71356e6b2498d
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-05 03:26:10 +0800

    access last round andd push guess

--- backend/handlers/makeGuess.js
+++ backend/handlers/makeGuess.js
@@ -6,13 +6,27 @@ export const makeGuessHandler = (request, response) => {
         return
     }
 
-    if(!request.body.player){
+    if(! request.body.player){
         response.status(400).send({message: "No 'Player' property sent"})
         return
     }
 
-    if(!request.body.guess) {
+    if(request.body.guess === undefined) {
         response.status(400).send({messae: "No 'Guess' property sent"})
         return
     }
+
+    const currentRoundId = gameState.rounds[gameState.rounds.length -1].id
+
+    if(! gameState.guesses[currentRoundId]) {
+        gameState.guesses[currentRoundId] = []
+    }
+
+    gameState.guesses[currentRoundId].push({
+        player: request.body.player,
+        guess: request.body.guess
+    })
+
+    response.status(201).send(gameState)
+
 }
