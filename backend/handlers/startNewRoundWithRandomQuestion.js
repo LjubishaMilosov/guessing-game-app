2c66bb53fa5928bee94ab54bae443808b63cb0c0
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-04 18:21:16 +0800

    select random question

--- backend/handlers/startNewRoundWithRandomQuestion.js
+++ backend/handlers/startNewRoundWithRandomQuestion.js
@@ -3,15 +3,18 @@ import { v4 as uuidv4 } from "uuid"
 import { questions } from "../questions.js"
 
 export const startNewRoundWithRandomQuestion = (request, response) => {
+    
+    const randomIndex = Math.floor(Math.random() * questions.length)
+    
     const newRound = {
         id: uuidv4(),
-        questionId: questions[1].id,
+        questionId: questions[randomIndex].id,
     }
 
     gameState.rounds.push(newRound)
 
     return response.status(200).send({
         round: newRound,
-        question: questions[1]
+        question: questions[randomIndex],
     })
 }
