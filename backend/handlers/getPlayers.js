f2de013bc0cbc0a3f3fced75370cb43dde7f50c3
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-08 05:32:21 +0800

    sorting the final score

--- backend/handlers/getPlayers.js
+++ backend/handlers/getPlayers.js
@@ -2,5 +2,8 @@ import { gameState } from "../gameState/index.js"
 
 
 export const getPlayersHandler = (req, res) => {
+    gameState.players.sort( (a,b) => {
+        return b.points - a.points
+    })
     res.status(200).send(gameState.players)
 }
