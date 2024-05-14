03372565e9b20406f05e38856432877c22cb188f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2024-05-14 22:33:55 +0800

    generating a list of JSX elements with .map()

---
+++ exploring/map.js
@@ -0,0 +1,7 @@
+const friends = ["Rachel", "Phoebe", "Ross", "Monica", "Chandler", "Joey"]
+
+const initials = friends.map((friend) => {
+    return friend[0]
+})
+
+console.log(initials)
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,29 @@
 import Link from "next/link"
 
 export default function Home() {
+
+  const players = [
+    {
+      name: "Ljubisha",
+      points: 0
+    },
+    {
+      name: "Jenny",
+      points: 0
+    }
+  ]
+
   return(
     <div>
     <h1>Guessing Game</h1>
     <p> Waiting for players. Currently joined: </p>
+    {
+      players.map((player) => {
+        return (
+          <p>{player.points}</p>
+        )
+      })
+    }
     <h2> Join</h2>
     <p>Navigate to <Link href="/join">/Join</Link> on your phone to join the game!</p>
     </div>
